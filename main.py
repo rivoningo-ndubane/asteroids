f18782f92bd6bd6da8bb0e85664b48a882e9db87
rivoningo-ndubane <rivinine@gmail.com>
2024-11-10 22:20:28 +0200

    A. add circle shape class

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ def main():
     # initialize pygame
     pygame.init()
 
+    # Create a screen for the game
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create a clock to control FPS
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -19,6 +24,9 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
+
+        # Limit the frames to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
